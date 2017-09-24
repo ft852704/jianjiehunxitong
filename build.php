a9fe0470d51b8b9093bf3e3c451be459b6133eff
hyh <ft852704@qq.com>
2017-09-24 15:32:48 +0800

    初始化项目

--- build.php
+++ build.php
@@ -13,13 +13,31 @@ return [
     // 生成应用公共文件
     '__file__' => ['common.php', 'config.php', 'database.php'],
 
-    // 定义demo模块的自动生成 （按照实际定义的文件名生成）
-    'demo'     => [
-        '__file__'   => ['common.php'],
+    // 后台模块
+    'admin'     => [
+        '__file__'   => ['common.php', 'config.php', 'database.php'],
         '__dir__'    => ['behavior', 'controller', 'model', 'view'],
-        'controller' => ['Index', 'Test', 'UserType'],
-        'model'      => ['User', 'UserType'],
+        'controller' => ['Index'],
+        'model'      => [],
         'view'       => ['index/index'],
     ],
+    //职业人
+    // professional
+	'index'     => [
+        '__file__'   => ['common.php', 'config.php', 'database.php'],
+        '__dir__'    => ['behavior', 'controller', 'model', 'view'],
+        'controller' => ['Index'],
+        'model'      => [],
+        'view'       => ['index/index'],
+    ],
+	
+	// 前台模块
+	'index'     => [
+        '__file__'   => ['common.php', 'config.php', 'database.php'],
+        '__dir__'    => ['behavior', 'controller', 'model', 'view'],
+        'controller' => ['Index'],
+        'model'      => [],
+        'view'       => ['index/index'],
+    ]
     // 其他更多的模块定义
 ];
